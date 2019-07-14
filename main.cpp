0acfd6946eb4114ecf5114b66d6776fa2427a3b0
Koushiq <koushiq1234@gmail.com>
2019-07-14 19:19:19 +0600

    second commit

--- main.cpp
+++ main.cpp
@@ -6,18 +6,32 @@
 
 using namespace std;
 
+GLfloat h;
+GLfloat m;
+GLfloat s;
 
 void display();
 
+void idle();
+
 void drawGrid();
 
-void drawQuad();
+void drawQuad(GLfloat , GLfloat , GLfloat , GLfloat , GLfloat , GLfloat , GLfloat , GLfloat);
+void drawLine(GLfloat , GLfloat , GLfloat , GLfloat);
+void drawTriangle(GLfloat , GLfloat , GLfloat , GLfloat , GLfloat , GLfloat);
 
 void drawCircle(GLfloat,GLfloat,GLfloat);
 
 void drawDigits(GLfloat,GLfloat,GLfloat);
 
 void drawBitmapText(char *str,float x,float y,float z);
+
+
+
+void idle()
+{
+    glutPostRedisplay();
+}
 
 void drawBitmapText(char *str,float x,float y,float z)
 {
@@ -105,6 +119,29 @@ void drawQuad(GLfloat x0, GLfloat y0, GLfloat x1, GLfloat y1, GLfloat x2, GLfloa
 }
 
 
+void drawLine(GLfloat x0, GLfloat y0, GLfloat x1, GLfloat y1)
+{
+    glBegin(GL_LINES);
+
+    glVertex2f(x0,y0);
+
+    glVertex2f(x1,y1);
+
+    glEnd();
+}
+
+void drawTriangle(GLfloat x0, GLfloat y0, GLfloat x1, GLfloat y1, GLfloat x2, GLfloat y2)
+{
+    glBegin(GL_TRIANGLES);
+
+    glVertex2f(x0,y0);
+
+    glVertex2f(x1,y1);
+
+    glVertex2f(x2,y2);
+
+    glEnd();
+}
 
 void display()
 {
@@ -116,16 +153,55 @@ void display()
 
 	drawCircle(0.0f,0.0f,0.8f);
 
-	glColor3ub(230, 230, 230);
+	glColor3ub(153, 153, 255);
 
 	drawCircle(0.0f,0.0f,0.75f);
 
-	drawGrid();// makes a 2D grid
+	//drawGrid();// makes a 2D grid
 	glColor3ub(38, 38, 38);
+
 	drawDigits(-0.02,0.0,0.65);
 
-	glFlush();
 
+	//rotate hour pointer
+	glPushMatrix();
+    glRotatef(h,0,0,1);
+	glColor3ub(230, 230, 0);//hour pointer
+	drawQuad(0.2,0.055, -0.10,0.0, 0.2,-0.055, 0.48,00);
+	glLoadIdentity();
+	glPopMatrix();
+
+//rotate minute pointer
+	glPushMatrix();
+    glRotatef(m,0,0,1);
+	glColor3ub(179, 0, 0);//minute pointer
+	drawQuad(0.0,0.6 , -0.055,0.35, 0.0,-0.13, 0.055,0.35);
+	glLoadIdentity();
+	glPopMatrix();
+
+//rotate second pointer
+
+    glPushMatrix();
+    glRotatef(s,0,0,1);
+	glColor3ub(38,38,38);//minute pointer
+	drawQuad(0.02,0.60, -0.02,0.60, -0.02,-0.15 ,0.02,-0.15);
+	glLoadIdentity();
+	glPopMatrix();
+
+
+
+//	pointer holder
+
+	glColor3ub(153, 0, 0);
+	drawCircle(0.0f,0.0f,0.050f);
+	glColor3ub(230, 0, 115);
+	drawCircle(0.0f,0.0f,0.035f);
+
+	//decrement
+	s-=0.0044;
+	m-=0.00044;
+	h-=0.000044;
+	glFlush();
 }
 
 
@@ -136,6 +212,7 @@ int main(int argc, char** argv)
     glutInitWindowPosition(1920/2 - 210,1080/2 - 210);
     glutCreateWindow("Test");
     glutDisplayFunc(display);
+    glutIdleFunc(idle);
     glutMainLoop();
     return 0;
 }
